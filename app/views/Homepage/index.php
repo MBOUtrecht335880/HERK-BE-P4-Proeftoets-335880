69c11340681e96e5f2a3518e5286c1071f55d2db
pershiyasharifi <335880@student.mboutrecht.nl>
2023-06-13 15:13:34 +0200

    created db

---
+++ app/views/Homepage/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <h3><?= $data['title']; ?></h3>
+    <a href="<?= URLROOT; ?>/Leerling/overzichtrijlespakketen">Overzicht Rijles Pakketen Leerlingen</a>
+</body>
+
+</html>
